047a3389803ba0743abafb546ec3e043e69101aa
kumano-jimushitsu <kumano.jimushitsu.gmail>
2022-02-20 14:58:03 +0900

    Git cache 初期化やり直し

---
+++ Operation.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace RegisterParcelsFromPC
+{
+    public class Operation
+    {
+
+        public string connStr;
+        public Operation(string connStr)
+        {
+            this.connStr = connStr;
+        }
+
+
+        public void execute_sql(string sqrstr)
+        {
+
+            using (var conn = new SqlConnection(connStr))
+            {
+                try
+                {
+                    //接続
+                    conn.Open();
+
+                    //コマンド実行
+                    SqlCommand command_register = new SqlCommand(sqrstr, conn);
+                    command_register.ExecuteNonQuery();
+
+                    //接続断
+                    conn.Close();
+                }catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+        }
+
+        public List<string> get_all_parcels_uid(string sqlstr)
+        {//受取時：ある寮生名義の事務室にある荷物の荷物番号をすべて取得しList<int>で返す→11/3 stringで返すよう変更
+         //定期チェック時：削除可能なイベントのuidをすべて取得しListで返す
+            List<string> CurrentParcels = new List<string>();
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            try
+            {
+                SqlCommand com = new SqlCommand(sqlstr, con);
+                SqlDataReader sdr = com.ExecuteReader();
+
+                while (sdr.Read() == true)
+                {
+                    string uid = (string)sdr["uid"];
+                    CurrentParcels.Add(uid);
+                }
+                sdr.Close();
+                com.Dispose();
+            }catch(Exception e)
+            {
+                NLogService.PrintInfoLog("例外_getalluid");
+
+                NLogService.PrintInfoLog(e.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+            return CurrentParcels;
+        }
+        public List<string> get_all_event_uid(string sqlstr)
+        {//受取時：ある寮生名義の事務室にある荷物の荷物番号をすべて取得しList<int>で返す→11/3 stringで返すよう変更
+         //定期チェック時：削除可能なイベントのuidをすべて取得しListで返す
+            List<string> CurrentEvents = new List<string>();
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            try
+            {
+                SqlCommand com = new SqlCommand(sqlstr, con);
+                SqlDataReader sdr = com.ExecuteReader();
+
+                while (sdr.Read() == true)
+                {
+                    string uid = (string)sdr["uid"];
+                    CurrentEvents.Add(uid);
+                }
+                sdr.Close();
+                com.Dispose();
+            }
+            catch (Exception e)
+            {
+                NLogService.PrintInfoLog("例外_getalluid");
+
+                NLogService.PrintInfoLog(e.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+            return CurrentEvents;
+        }
+        public string select_one_xx(string sqlstr, string xx)
+        {
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            string uid = null ;
+            //int uidint = 0;
+            try
+            {
+                SqlCommand com = new SqlCommand(sqlstr, con);
+                SqlDataReader sdr = com.ExecuteReader();
+                sdr.Read();
+
+                if (sdr[xx] == DBNull.Value)
+                {
+                    uid = "null";
+                }
+                else
+                {
+                    uid = (string)sdr[xx];
+                    //uidint = (int)sdr[xx];
+                }
+
+
+                sdr.Close();
+                com.Dispose();
+            }
+            catch(Exception e)
+            {
+                NLogService.PrintInfoLog("例外_selectonexx");
+
+                NLogService.PrintInfoLog(e.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+            return uid;
+        }
+        public int select_one_xx_int(string sqlstr, string xx)
+        {
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            int uid = 0;
+            //int uidint = 0;
+            try
+            {
+                SqlCommand com = new SqlCommand(sqlstr, con);
+                SqlDataReader sdr = com.ExecuteReader();
+                sdr.Read();
+
+                if (sdr[xx] == DBNull.Value)
+                {
+                    uid = 0;
+                }
+                else
+                {
+                    uid = (int)sdr[xx];
+                    //uidint = (int)sdr[xx];
+                }
+
+
+                sdr.Close();
+                com.Dispose();
+            }
+            catch (Exception e)
+            {
+                NLogService.PrintInfoLog("例外_selectonexx");
+
+                NLogService.PrintInfoLog(e.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+            return uid;
+        }
+        public string calculate_registered_time(List<string> ParcelID, DateTime now, string owner_uid)//ここはリファクタリングなど一切していない
+        {
+            List<DateTime> RegiTime_list = new List<DateTime>();
+            string total_past_waittime = "";
+            string ParcelID_List = "";
+            foreach (string aParcelID in ParcelID)
+            {
+                ParcelID_List += "'";
+                ParcelID_List += aParcelID.ToString();
+                ParcelID_List += "'";
+                ParcelID_List += ",";
+            }
+            ParcelID_List = ParcelID_List.Trim(',');
+
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            try
+            {
+                //荷物の登録時間をすべて取得し、regi_time_listに格納
+                string sqlstr = $@"select register_datetime from parcels where uid in ({ParcelID_List})";
+                SqlCommand com = new SqlCommand(sqlstr, con);
+                SqlDataReader sdr = com.ExecuteReader();
+
+                while (sdr.Read() == true)
+                {
+                    DateTime regi_time = (DateTime)sdr["register_datetime"];
+                    RegiTime_list.Add(regi_time);
+                }
+
+                sdr.Close();
+                com.Dispose();
+
+                //寮生のこれまでの累計時間を取得し、total_past_waittimeに格納
+                string sqlstr2 = $@"select parcels_total_waittime from ryosei where uid='{owner_uid}'";
+                SqlCommand com2 = new SqlCommand(sqlstr2, con);
+                SqlDataReader sdr2 = com2.ExecuteReader();
+
+                while (sdr2.Read() == true)
+                {
+                    total_past_waittime = (string)sdr2["parcels_total_waittime"];
+                }
+
+                sdr2.Close();
+                com2.Dispose();
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            //もっといいやり方があるかもしれないが、一旦これで良し
+            int total_second = 0;
+            //これまでの累計時間を秒になおして格納
+            string[] past_working = total_past_waittime.Split(':');//23:15:24 23時間15分24秒
+            total_second += int.Parse(past_working[0]) * 3600;
+            total_second += int.Parse(past_working[1]) * 60;
+            total_second += int.Parse(past_working[2]);
+
+            //今回取得し加算する時間を秒に直して格納
+            TimeSpan totalTimeSpan = new TimeSpan(0, 0, 0, 0);//{370.06:16:25.6079800} 370日6時間16分25秒  24時間を超えない場合は{00:00:03.8976284}のように、～日の部分がないので処理を変える
+            foreach (DateTime aRegiTime in RegiTime_list)
+            {
+                totalTimeSpan += now - aRegiTime;
+            }
+
+            string[] working1 = totalTimeSpan.ToString().Split('.');//"370.06:16:25.6079800" 
+            if (working1.Length == 3)//24時間を超える場合
+            {
+                total_second += int.Parse(working1[0]) * 24 * 60 * 60;
+                string[] working2 = working1[1].Split(':');//06.16.25
+                total_second += int.Parse(working2[0]) * 60 * 60;
+                total_second += int.Parse(working2[1]) * 60;
+                total_second += int.Parse(working2[2]);
+
+            }
+            else//2しかあり得ないはず、24時間を超えない場合
+            {
+                string[] working2 = working1[0].Split(':');//06.16.25
+                total_second += int.Parse(working2[0]) * 60 * 60;
+                total_second += int.Parse(working2[1]) * 60;
+                total_second += int.Parse(working2[2]);
+            }
+
+            //秒数を、hour:min:secの形のstringに変換
+            string new_totalTime = "";
+            new_totalTime += (total_second / 3600).ToString();
+            new_totalTime += ":";
+            total_second %= 3600;
+            new_totalTime += (total_second / 60).ToString("00");
+            new_totalTime += ":";
+            total_second %= 60;
+            new_totalTime += total_second.ToString("00");
+            return new_totalTime;
+        }
+        public void calculate_registered_time2(string parcelID)//ここはリファクタリングなど一切していない
+        {
+            DateTime regi_time, rele_time;
+            string owner_uid;
+            string total_past_waittime;
+
+            SqlConnection con = new SqlConnection(connStr);
+            con.Open();
+            try
+            {
+                //荷物の登録時間をすべて取得し、regi_time_listに格納
+                string sqlstr = $@"select register_datetime,release_datetime,owner_uid from parcels where uid ='{parcelID}'";
+                SqlCommand com = new SqlCommand(sqlstr, con);
+                SqlDataReader sdr = com.ExecuteReader();
+
+                sdr.Read();
+
+                owner_uid = (string)sdr["owner_uid"];
+                regi_time = (DateTime)sdr["register_datetime"];
+                rele_time = (DateTime)sdr["release_datetime"];
+
+
+                sdr.Close();
+                com.Dispose();
+
+                //寮生のこれまでの累計時間を取得し、total_past_waittimeに格納
+                string sqlstr2 = $@"select parcels_total_waittime from ryosei where uid='{owner_uid}'";
+                SqlCommand com2 = new SqlCommand(sqlstr2, con);
+                SqlDataReader sdr2 = com2.ExecuteReader();
+
+                sdr2.Read();
+
+                total_past_waittime = (string)sdr2["parcels_total_waittime"];
+
+
+                sdr2.Close();
+                com2.Dispose();
+
+
+                //もっといいやり方があるかもしれないが、一旦これで良し
+                int total_second = 0;
+                //これまでの累計時間を秒になおして格納
+                string[] past_working = total_past_waittime.Split(':');//23:15:24 23時間15分24秒
+                total_second += int.Parse(past_working[0]) * 3600;
+                total_second += int.Parse(past_working[1]) * 60;
+                total_second += int.Parse(past_working[2]);
+
+                //今回取得し加算する時間を秒に直して格納
+                TimeSpan totalTimeSpan = new TimeSpan(0, 0, 0, 0);//{370.06:16:25.6079800} 370日6時間16分25秒  24時間を超えない場合は{00:00:03.8976284}のように、～日の部分がないので処理を変える
+
+                totalTimeSpan += rele_time - regi_time;
+
+                string[] working1 = totalTimeSpan.ToString().Split('.');//"370.06:16:25.6079800" もしくは　"06:16:25.6079800"
+                if (working1.Length == 3)//24時間を超える場合
+                {
+                    total_second += int.Parse(working1[0]) * 24 * 60 * 60;
+                    string[] working2 = working1[1].Split(':');//06.16.25
+                    total_second += int.Parse(working2[0]) * 60 * 60;
+                    total_second += int.Parse(working2[1]) * 60;
+                    total_second += int.Parse(working2[2]);
+
+                }
+                else//2しかあり得ないはず、24時間を超えない場合
+                {
+                    string[] working2 = working1[0].Split(':');//06.16.25
+                    total_second += int.Parse(working2[0]) * 60 * 60;
+                    total_second += int.Parse(working2[1]) * 60;
+                    total_second += int.Parse(working2[2]);
+                }
+
+                //秒数を、hour:min:secの形のstringに変換
+                string new_totalTime = "";
+                new_totalTime += (total_second / 3600).ToString();
+                new_totalTime += ":";
+                total_second %= 3600;
+                new_totalTime += (total_second / 60).ToString("00");
+                new_totalTime += ":";
+                total_second %= 60;
+                new_totalTime += total_second.ToString("00");
+                MakeSQLCommand make = new MakeSQLCommand();
+                string sql = make.toUpdate_ryosei_totalwaittime(owner_uid, new_totalTime);
+                execute_sql(sql);
+
+            }
+            catch(Exception e)
+            {
+                NLogService.PrintInfoLog("例外_calculated_register_datetime2");
+
+                NLogService.PrintInfoLog(e.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+    }
+
+}
---
+++ Periodic_check.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+
+namespace RegisterParcelsFromPC
+{
+    partial class Periodic_check : Form1
+    {
+        MakeSQLCommand sqlstr = new MakeSQLCommand();
+
+
+        public void periodical_check()
+        {
+            try
+            {
+
+                //10秒に一度呼ばれる
+                //deletable_eventを更新する
+                //具体的に言うと、登録もしくは受取イベントで、is_finishedが0であり、5分以上経過しているイベントに対して
+                //・slackを送信する（荷物受け取り引き渡しはryosei_uidとparcel_uid、slack_idの3点が必要、本人確認はparcel_uidは不要）
+                //・is_finishedを1にする
+
+                DateTime dt = DateTime.Now;
+                int deletable_time = int.Parse(ConfigurationManager.AppSettings["deletable_time"]);
+                string real_time = dt.ToString("yyyy-MM-dd HH:mm:ss");//実際の時間
+                string base_time = dt.AddSeconds(-1 * deletable_time).ToString("yyyy-MM-dd HH:mm:ss");//操作取り消しの時間を考慮した時間
+
+                List<string> target_uid = ope.get_all_parcels_uid(sqlstr.toGetList_PeriodicCheck(base_time,"1"));//先に受け取りの更新対象の一覧（parcel_uidの一覧）を取得
+                target_uid.AddRange(ope.get_all_parcels_uid(sqlstr.toGetList_PeriodicCheck(base_time, "2")));//先に引き渡しの更新対象の一覧（parcel_uidの一覧）を取得
+                target_uid.AddRange(ope.get_all_parcels_uid(sqlstr.toGetList_PeriodicCheck(real_time, "20")));
+                foreach (string an_event in target_uid)
+                {
+                    string msg;
+                    switch (get_eventtype_from_eventuid(an_event))
+                    {
+                        case "1":
+                            msg = "こんあじり！　事務室に荷物が届いたよ！ (" + get_registerdatetime_from_eventuid(an_event) + ")";
+                            send_slack(get_parceluid_from_eventuid(an_event), msg, 1);
+
+                            break;
+                        case "2":
+                            string parceluid = get_parceluid_from_eventuid(an_event);
+                            msg = "荷物が受取されたよ！。(" + get_registerdatetime_from_eventuid(an_event) + "事務室到着分)";
+                            send_slack(parceluid, msg, 2);
+                            ope.calculate_registered_time2(parceluid);
+                            break;
+                        case "20":
+                            msg = "本人確認が完了したよ！これから荷物が来たらお知らせするね！よろしく！";
+                            send_slack_complete_identify(get_ryoseiuid_from_eventuid(an_event), msg,20);
+                            break;
+                    }
+
+
+                }
+
+
+                ope.execute_sql(sqlstr.toPeriodicCheck(base_time,"1"));//is_finished=1にするupdate文をかます
+                ope.execute_sql(sqlstr.toPeriodicCheck(base_time, "2"));//is_finished=1にするupdate文をかます
+                ope.execute_sql(sqlstr.toPeriodicCheck(real_time, "20"));//is_finished=1にするupdate文をかます
+
+
+                //更新対象があったときのみ、イベントテーブルを再描画
+                if (target_uid.Count > 0)
+                {
+                    show_parcels_eventTable();
+                }
+
+
+                //一時間に一度通信をする（slackのトークン無効時間がよくわからないため）
+                DateTime now = DateTime.Now;
+                if (int.Parse(now.Minute.ToString()) == 0 && int.Parse(now.Second.ToString()) < 10)
+                {
+                    Httppost httppost = new Httppost();
+                    string user_code = ConfigurationManager.AppSettings["slack_testuser"];
+                    string message_str = $"定期通信だよ！";
+                    httppost.posting_DM(user_code, message_str);
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+
+                //数秒に一回起動してるので、インスタンスを破棄しないとメモリ食いまくってそのうち落ちる
+                Dispose();
+            }
+
+        }
+        public string get_eventtype_from_eventuid(string event_uid)
+        {
+
+            string event_type = ope.select_one_xx(sqlstr.toSelect_eventtype_from_eventuid(event_uid), "event_type");
+            return event_type;
+        }
+        public string get_registerdatetime_from_eventuid(string event_uid)
+        {
+
+            string parcel_uid = ope.select_one_xx(sqlstr.toSelect_registerdatetime_from_eventuid(event_uid), "created_at");
+            return parcel_uid;
+        }
+
+        public string get_ryoseiuid_from_eventuid(string event_uid)
+        {
+            //event uidをキーとしてryosei_uidを取得する
+
+            string ryosei_uid = ope.select_one_xx(sqlstr.toSelect_ryoseiuid_from_eventuid(event_uid), "ryosei_uid");
+            return ryosei_uid;
+        }
+
+        public string get_parceluid_from_eventuid(string event_uid)
+        {
+            //event uidをキーとしてparcels_uidを取得する
+
+            string parcel_uid = ope.select_one_xx(sqlstr.toSelect_parceluid_from_eventuid(event_uid), "parcel_uid");
+            return parcel_uid;
+        }
+
+        public void send_slack(string parcel_uid, string message_str, int event_type)
+        {
+            //QRコードを一時的に作成するパスの名前
+
+            string nowtime = DateTime.Now.ToString("HH-mm-ss");
+            string filename = $@"C:\temp\{nowtime}.gif";
+
+
+            //parcel uidをキーとしてowner_uidを取得,owner_uidをキーとしてslack_uidを取得する
+
+            string owner_uid = ope.select_one_xx(sqlstr.toSelect_ryoseiuid_from_parceluid(parcel_uid), "owner_uid");
+            string slack_uid = ope.select_one_xx(sqlstr.toSelect_slackid_from_ryoseiuid(owner_uid), "slack_id");
+            int ryosei_status = ope.select_one_xx_int(sqlstr.toSelect_status_from_ryoseiuid(owner_uid), "status");
+            if (slack_uid == "" || slack_uid == "null"||ryosei_status==5) return;
+
+            Httppost httppost = new Httppost();
+            switch (event_type)
+            {
+                case 1:
+                    //qrコード作成
+                    QrCode qr = new QrCode();
+                    qr.QRcodeCreate(parcel_uid, filename);
+
+                    //slackに送信
+                    //"U026B4NDH0T";//俺のやつ
+                    //"U026C106W0K"; //わたるの
+                    httppost.posting_DM_image(slack_uid, filename, message_str);
+
+                    File.Delete(filename);
+
+                    break;
+                case 2:
+                    httppost.posting_DM(slack_uid, message_str);
+                    break;
+            }
+
+        }
+
+        //parcels_uidではなくowrner_uidから送信する。
+        public void send_slack_complete_identify(string owner_uid, string message_str, int event_type)
+        {
+            //QRコードを一時的に作成するパスの名前
+
+            string nowtime = DateTime.Now.ToString("HH-mm-ss");
+            string filename = $@"C:\temp\{nowtime}.gif";
+
+
+            //parcel uidをキーとしてowner_uidを取得,owner_uidをキーとしてslack_uidを取得する
+
+            string slack_uid = ope.select_one_xx(sqlstr.toSelect_slackid_from_ryoseiuid(owner_uid), "slack_id");
+            int ryosei_status = ope.select_one_xx_int(sqlstr.toSelect_status_from_ryoseiuid(owner_uid), "status");
+            if (slack_uid == "" || slack_uid == "null" || ryosei_status == 5) return;
+
+            Httppost httppost = new Httppost();
+            switch (event_type)
+            {
+                
+                case 20:
+                    httppost.posting_DM(slack_uid, message_str);
+                    break;
+            }
+
+        }
+    }
+}
